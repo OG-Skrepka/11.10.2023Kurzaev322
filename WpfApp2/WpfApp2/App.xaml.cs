c9f35e530ffcf37560d4102798b553fd99f882df
OG-Skrepka <reverse125game@gmail.com>
2023-10-13 09:57:46 +0300

    initialiazation of project

---
+++ WpfApp2/WpfApp2/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp2.Components;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Hardwareshop1Entities db = new Hardwareshop1Entities ();
+    }
+}
